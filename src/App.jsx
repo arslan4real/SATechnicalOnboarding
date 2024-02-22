fcddbeeb77256aebd75b9c14cc15784b14328617
Arslan Khalid <arslankhalid5798@gmail.com>
2024-02-22 07:07:41 +0500

    Names

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Arslan Khalid",
+  title: "UI/UX & Product Designer",
+  email: "arslankhalid5798@gmail.com",
+  gitHub: "arslan4real",
+  instagram: "arslanxdesign",
+  linkedIn: "arslan4real",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "arslan4real_",
+  youTube: "arslan4real",
 };
 
 const primaryColor = "#4E567E";
